603643adcb2b92a0001343b53d98896013e45276
sachinpandeygurh <sachingurh@gmail.com>
2023-09-19 19:12:55 +0530

    initial commit

--- src/components/Management.js
+++ src/components/Management.js
@@ -6,16 +6,16 @@ import TenentData from './TenentData'
 const Management = () => {
   return (
     <div className='mt-8'>
-        <TenentData toolicon={faUserTie} paira="New Tenent info"  bg="bg-white" text="text-brand"/>
-        <TenentData toolicon={faDollarSign} paira="New Tenent info"  bg="bg-white" text="text-brand"/>
+        <TenentData toolicon={faUserTie} paira="New Tenent info"  bg="bg-white" text="text-brand" otherclass=''/>
+        <TenentData toolicon={faDollarSign} paira="New Tenent info"  bg="bg-white" text="text-brand" otherclass=''/>
       
 <div className='row container m-auto'>
-<TenentData col='col-md-2' width="" border='none' otherclass="mx-md-1"/>
-<TenentData toolicon={faIgloo} paira="New Tenent info" col='col-md-3' width="" bg="bg-white" text="text-brand"/>
-<TenentData toolicon={faPlus} paira="New Tenent info" col='col-md-3' width="" bg="bg-white" text="text-brand"/>
-<TenentData col='col-md-2' width="" border='none' otherclass="mx-md-1" />
+<TenentData col='col-md-2 col-1 ' width="" border='none' otherclass="mx-1"/>
+<TenentData toolicon={faIgloo} paira="New Tenent info" col='col-md-3 col-4' width="" bg="bg-white" text="text-brand"/>
+<TenentData toolicon={faPlus} paira="New Tenent info" col='col-md-3 col-4' width="" bg="bg-white" text="text-brand"/>
+<TenentData col='col-md-2 col-1 ' width="" border='none' otherclass="mx-1" />
 </div>
-<TenentData paira="Home page" bg="bg-brand" text="text-white"/>
+<TenentData paira="Home page" bg="bg-brand" text="text-white" otherclass='w-md-50 w-75'/>
     </div>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,22 @@
-import logo from '../img/navlogo.png';
-import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-    faHouse,faUserTie
-  } from "@fortawesome/free-solid-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faUserTie } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faUserTie } from "@fortawesome/free-solid-svg-icons";
+import logo from "../img/navlogo.png";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Nav() {
+  const [activeLink, setActiveLink] = useState(false);
+
+  const handleLinkClick = (to) => {
+    setActiveLink(to);
+  };
+
   return (
-    <nav className="navbar border navbar-expand-lg navbar-dark bg-white px-md-3 navbar fixed-top ">
-      <Link className="navbar-brand" to="/">
+    <nav className="navbar border navbar-expand-lg navbar-dark bg-white px-md-3 navbar fixed-top">
+      <Link className="navbar-brand d-none d-md-block" to="/">
         <img src={logo} alt="logo" width="40" height="40" className="mr-2" />
-      
       </Link>
 
       <button
@@ -28,26 +31,47 @@ function Nav() {
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse justify-content-end px-5" id="navbarNav">
+      <div
+        className="collapse navbar-collapse justify-content-end px-5"
+        id="navbarNav"
+      >
         <ul className="navbar-nav align-items-center ml-auto">
           <li className="nav-item">
-            <Link className="nav-link text-dark fw-bold text-brand" to="/">
-            <FontAwesomeIcon icon={faHouse} /> Homepage
+            <Link
+              className={`nav-link text-dark fw-bold ${
+                activeLink === "/" ? "active" : ""
+              }`}
+              to="/"
+              onClick={() => handleLinkClick("/")}
+            >
+              <FontAwesomeIcon icon={faHouse} /> Homepage
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-dark fw-bold" to="/tenent">
+            <Link
+              className={`nav-link text-dark fw-bold ${
+                activeLink === "/tenent" ? "active" : ""
+              }`}
+              to="/tenent"
+              onClick={() => handleLinkClick("/tenent")}
+            >
               Tenent
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-dark fw-bold" to="/management">
+            <Link
+              className={`nav-link text-dark fw-bold ${
+                activeLink === "/management" ? "active" : ""
+              }`}
+              to="/management"
+              onClick={() => handleLinkClick("/management")}
+            >
               Management
             </Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/">
-            <FontAwesomeIcon className="text-brand fa-2x"  icon={faUserTie} />
+              <FontAwesomeIcon className="text-brand fa-2x" icon={faUserTie} />
             </Link>
           </li>
         </ul>
